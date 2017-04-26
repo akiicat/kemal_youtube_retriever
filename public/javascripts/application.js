ac24bd09d9d859e8f3f02f54248d71d15e8ad383
akiicat <aaaa1379@gmail.com>
2017-04-26 20:02:28 +0800

    use es6 and parse title

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,48 +1,58 @@
-var YoutubeRetriever = React.createClass({
-  getInitialState: function () {
-    return {
+class YoutubeRetriever extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      title: "hello",
+      length: "0",
     };
-  },
+    this.video = {
+      url: "",
+      id: "",
+    }
+  }
 
-  getVideoId: function() {
-    var video_url = document.getElementById('video_url').value
+  getVideoId() {
     var video_id_m = /^((?:https?:\/\/|\/\/)(?:(?:(?:(?:www\.)?youtube\.com\/)(?:(?:(?:v|embed|e)\/(?!videoseries))|(?:(?:(?:watch|movie)\/?)?(?:\?)v=)))|(?:youtu\.be)\/))?([0-9A-Za-z_-]{11})/m
-    var video_id = video_id_m.exec(video_url)[2]
-    return video_id
-  },
+    this.video.url = document.getElementById('video_url').value
+    this.video.id = video_id_m.exec(this.video.url)[2]
+    return this.video.id
+  }
 
-  sendVideoUrl: function() {
+  sendVideoUrl() {
     var xhr = null;
-    if(window.XMLHttpRequest){
+    if (window.XMLHttpRequest) {
       xhr = new XMLHttpRequest();
-    } else {
+    }
+    else {
       xhr = new ActiveXObject('Microsoft.XMLHTTP');
     }
 
-    xhr.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById('video_info').innerHTML = this.responseText;
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        // document.getElementById('video_info').innerHTML =
+        var rsp = JSON.parse(xhr.responseText)
+        this.state.title = rsp.title
+        this.forceUpdate()
       }
     };
 
-    video_id = this.getVideoId()
-    xhr.open('GET', '/watch?v=' + video_id,  true);
+    this.getVideoId()
+    xhr.open('GET', '/watch?v=' + this.video.id,  true);
     xhr.send();
-  },
+  }
 
-  sendVideoUrlWithEnter: function (e) {
+  sendVideoUrlWithEnter(e) {
     if (e.keyCode == 13) {
       this.sendVideoUrl();
     }
-  },
+  }
 
-  render: function () {
+  render() {
     return React.createElement('div', null,
-      React.createElement('input', { id: 'video_url', autofocus: true, placeholder: 'Youtube URL', type: 'text', ref: 'video_url', onKeyUp: this.sendVideoUrlWithEnter }),
-      React.createElement('button', { type: 'button', onClick: this.sendVideoUrl }, 'Send'),
-      React.createElement('div', { id: 'video_info' }, null)
+      React.createElement('input', { id: 'video_url', autofocus: true, placeholder: 'Youtube URL', type: 'text', ref: 'video_url', onKeyUp: this.sendVideoUrlWithEnter.bind(this) }),
+      React.createElement('button', { type: 'button', onClick: this.sendVideoUrl.bind(this) }, 'Send'),
+      React.createElement('div', { id: 'video_info' }, this.state.title)
     );
   }
-});
-
+}
 ReactDOM.render(React.createElement(YoutubeRetriever, null), document.getElementById('youtube-retriever'));

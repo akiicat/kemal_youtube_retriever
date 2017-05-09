f33c810893042fee0edbc81d9c8ca20a174b56f6
akiicat <aaaa1379@gmail.com>
2017-05-09 17:42:52 +0800

    new feature api

--- src/kemal_youtube_retriever.cr
+++ src/kemal_youtube_retriever.cr
@@ -1,42 +1,86 @@
 require "kemal"
 require "youtube_retriever"
 
+LOG.level = Logger::ERROR
 BAD_REQUEST = {
   :status => "404",
   :message => "bad request"
 }
 
-before_get "/api/v1/watch" do |env|
-  env.response.content_type = "application/json; charset=utf-8"
-  request = env.request.resource.match(/\?(?<video_url>.+)/).try(&.["video_url"]).to_s
-  env.set "video_url", URI.decode_www_form(request)["v"]
+error 404 do
+  render "src/views/404.ecr"
 end
 
-before_get "/api/v1/youtube-dl" do |env|
+before_get "/api/v1/json/*" do |env|
   env.response.content_type = "application/json; charset=utf-8"
+end
+
+before_get "/api/v1/*" do |env|
   request = env.request.resource.match(/\?(?<video_url>.+)/).try(&.["video_url"]).to_s
-  env.set "video_url", URI.decode_www_form(request)["v"]
+  env.set "video_url", URI.decode_www_form(request)["v"]?
 end
 
 get "/" do |env|
   render "src/views/index.ecr"
 end
 
-get "/api/v1/watch" do |env|
+get "/api/v1/json/watch" do |env|
   begin
-    YoutubeRetriever.dump_json(env.get("video_url").to_s).to_json
+    Youtube::Retriever.dump_json(env.get("video_url").to_s).to_json
   rescue
     BAD_REQUEST.to_json
   end
 end
 
-get "/api/v1/youtube-dl" do |env|
+get "/api/v1/json/video" do |env|
+  begin
+    Youtube::Retriever.get_video_urls(env.get("video_url").to_s).to_json
+  rescue
+    BAD_REQUEST.to_json
+  end
+end
+
+get "/api/v1/json/audio" do |env|
+  begin
+    Youtube::Retriever.get_audio_urls(env.get("video_url").to_s).to_json
+  rescue
+    BAD_REQUEST.to_json
+  end
+end
+
+get "/api/v1/json/video_info" do |env|
+  begin
+    Youtube::Retriever.video_info(env.get("video_url").to_s).to_json
+  rescue
+    BAD_REQUEST.to_json
+  end
+end
+
+get "/api/v1/json/youtube-dl" do |env|
   begin
     io = IO::Memory.new
     Process.run("youtube-dl", ["-j", env.get("video_url").to_s], output: io)
     JSON.parse(io.to_s).to_json
   rescue
     BAD_REQUEST.to_json
+  end
+end
+
+get "/api/v1/link/video" do |env|
+  begin
+    url = Youtube::Retriever.get_video_urls(env.get("video_url").to_s).first[:url]
+    env.redirect url
+  rescue
+    env.response.status_code = 404
+  end
+end
+
+get "/api/v1/link/audio" do |env|
+  begin
+    url = Youtube::Retriever.get_audio_urls(env.get("video_url").to_s).first[:url]
+    env.redirect url
+  rescue
+    env.response.status_code = 404
   end
 end
 
--- src/kemal_youtube_retriever/version.cr
+++
@@ -1,3 +0,0 @@
-module KemalYoutubeRetriever
-  VERSION = "0.1.0"
-end

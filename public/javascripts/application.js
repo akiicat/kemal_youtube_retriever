12f0cab15d0a6a471aac0f65ae65fcc7e9c0087d
Akiicat <aaaa1379@gmail.com>
2017-08-12 22:53:48 +0800

    use vue.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,129 +1,87 @@
-class YoutubeRetriever extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {};
-    this.video = {};
-  }
-
-  getVideoId() {
-    var video_id_m = /^((?:https?:\/\/|\/\/)(?:(?:(?:(?:www\.)?youtube\.com\/)(?:(?:(?:v|embed|e)\/(?!videoseries))|(?:(?:(?:watch|movie)\/?)?(?:\?)v=)))|(?:youtu\.be)\/))?([0-9A-Za-z_-]{11})/m
-    this.video.url = document.getElementById('video-url').value
-    this.video.id = video_id_m.exec(this.video.url)[2]
-    return this.video.id
-  }
-
-  sendVideoUrl() {
-    var xhr = null;
-    if (window.XMLHttpRequest) {
-      xhr = new XMLHttpRequest();
-    }
-    else {
-      xhr = new ActiveXObject('Microsoft.XMLHTTP');
-    }
-
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState == 4 && xhr.status == 200) {
-        var rsp = JSON.parse(xhr.responseText)
-
-        if (rsp.status == "404" || rsp.author == "") {
-          this.state.error = this.renderError()
-          this.forceUpdate()
-          return false
-        }
-
-        var date = new Date(null);
-        date.setSeconds(rsp.length_seconds);
-        this.state.error = null
-        this.state.title = rsp.title
-        this.state.author = rsp.author
-        this.state.length = date.toISOString().substr(11, 8);
-        this.state.streams = this.renderStreams(rsp.streams, "default")
-        this.state.video_only = this.renderStreams(rsp.streams, "video only")
-        this.state.audio_only = this.renderStreams(rsp.streams, "audio only")
-        this.forceUpdate()
-      }
-    };
-
-    this.getVideoId()
-    xhr.open('GET', '/api/v1/json/watch?v=' + this.video.id,  true);
-    xhr.send();
-  }
-
-  sendVideoUrlWithEnter(e) {
-    if (e.keyCode == 13) {
-      this.sendVideoUrl();
+Vue.component('streams-detail', {
+  props: ['video'],
+  template: `
+    <div class="detail">
+      <div>
+        <a :href="'https://www.youtube.com/watch?v=' + video.video_id">
+          <img :src="video.max_thumbnail_url"/>
+        </a>
+      </div>
+      <div>{{ video.title }}</div>
+      <div>{{ video.author }}</div>
+      <div>{{ time }}</div>
+    </div>
+  `,
+  computed: {
+    time () {
+      var date = new Date(null)
+      date.setSeconds(this.video.length_seconds)
+      return date.toISOString().substr(11, 8)
     }
   }
+})
 
-  renderError() {
-    return React.createElement('div', { className: 'paragraph' },
-      React.createElement('h4', { className: 'text-center' }, "Something Wrong" )
-    )
+
+Vue.component('streams-content', {
+  props: ['streams', 'title'],
+  template: `
+    <div class="content">
+      <h2>{{ title }}</h2>
+      <div v-for="stream in streams">
+        <a :href="stream.url" target="_blank">
+          <span>Download</span>
+          <span>{{ stream.container }}</span>
+          <span>{{ stream.video_resolution }}</span>
+          <span>{{ stream.audio_encoding }}</span>
+        </a>
+      </div>
+    </div>
+  `
+})
+
+Vue.component('github-footer', {
+  props: ['github_url'],
+  template: `
+    <footer>
+      <a :href="github_url">
+        <i class="icon github"></i>
+      </a>
+    </footer>
+  `
+})
+
+new Vue({
+  el: '#youtube',
+  data: {
+    url: "",
+    github_url: "https://github.com/akiicat/kemal_youtube_retriever",
+    video: {
+      title: "",
+      author: "",
+      length_seconds: "",
+      streams: []
+    }
+  },
+  computed: {
+  },
+  methods: {
+    getStreams (type = "default") {
+      return this.video.streams.filter(s => s.comment === type)
+    },
+    send () {
+      let video_id_m = /^((?:https?:\/\/|\/\/)(?:(?:(?:(?:www\.)?youtube\.com\/)(?:(?:(?:v|embed|e)\/(?!videoseries))|(?:(?:(?:watch|movie)\/?)?(?:\?)v=)))|(?:youtu\.be)\/))?([0-9A-Za-z_-]{11})/m
+      let video_id = video_id_m.exec(this.url)[2]
+      
+      axios.get('/api/v1/json/watch?v=' + video_id)
+        .then((response) => {
+            this.video = response.data
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
   }
+})
 
-  renderStreams(streams, selector) {
-    streams = streams.filter((stream) => {
-      return stream.comment == selector
-    })
+Vue.config.devtools = false
 
-    selector = (selector == "default") ? 'VIDEO + AUDIO' : selector.toUpperCase();
-
-    return React.createElement('div', {},
-      React.createElement('hr', {}, null),
-      React.createElement('h2', { className: 'text-center' }, selector),
-      React.createElement('table', {},
-        React.createElement('tbody', {}, this.renderStreamsBody(streams))
-      )
-    )
-  }
-
-  renderStreamsBody(streams) {
-    return streams.map((stream) => {
-      return React.createElement('tr', {},
-        // url, itag, container, video_resolution, video_profile, video_bitrate, video_encoding, audio_bitrate, audio_encoding, comment
-        React.createElement('td', {},
-          React.createElement('a', { href: stream.url + '&title=' + this.state.title, target: '_blank' }, 'Download')
-        ),
-        React.createElement('td', {}, stream.container),
-        React.createElement('td', {}, stream.video_resolution),
-        React.createElement('td', {}, stream.audio_encoding)
-      )
-    })
-  }
-
-  renderIcon(icon, link) {
-    return React.createElement('a', { href: link },
-      React.createElement('img', { className: 'icon', src: '/images/' + icon}, null)
-    )
-  }
-
-  render() {
-    return React.createElement('div', {},
-      React.createElement('header', {},
-        React.createElement('h1', { className: 'text-center' }, 'YOUTUBE RETRIEVER'),
-        React.createElement('div', { id: 'url-input', className: 'text-center' },
-          React.createElement('input', { id: 'video-url', autofocus: true, placeholder: 'Youtube URL', type: 'text', ref: 'video-url', onKeyUp: this.sendVideoUrlWithEnter.bind(this) }),
-          React.createElement('button', { type: 'button', onClick: this.sendVideoUrl.bind(this) }, 'Send')
-        )
-      ),
-      React.createElement('hr', {}, null),
-      React.createElement('div', { id: 'response-content' },
-        this.state.error,
-        React.createElement('div', { className: 'paragraph' },
-          React.createElement('div', { className: 'text-center title' }, this.state.title ),
-          React.createElement('div', { className: 'text-center title' }, this.state.author ),
-          React.createElement('div', { className: 'text-center title' }, this.state.length )
-        ),
-        React.createElement('div', { className: 'container' }, this.state.streams),
-        React.createElement('div', { className: 'container' }, this.state.video_only),
-        React.createElement('div', { className: 'container' }, this.state.audio_only)
-      ),
-      React.createElement('footer', {},
-        React.createElement('div', { className: 'container text-center' },
-          this.renderIcon('GitHub-Mark-Light-120px-plus.png', '//github.com/akiicat/kemal_youtube_retriever')
-        )
-      )
-    );
-  }
-}
-ReactDOM.render(React.createElement(YoutubeRetriever, null), document.getElementById('youtube-retriever'));
